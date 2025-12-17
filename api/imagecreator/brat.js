24c8df9daf38719b476050c24e2916e6a2074757
Matstoree <138416255+Matstoree@users.noreply.github.com>
2025-12-18 00:56:53 +0800

    Update brat.js

--- api/imagecreator/brat.js
+++ api/imagecreator/brat.js
@@ -1,47 +1,39 @@
+const axios = require('axios')
+
 module.exports = function app (app) {
 
   app.get('/imagecreator/brat', async (req, res) => {
     try {
       const { apikey, text } = req.query
-      if (!apikey || !text)
-        return res.json({ status: false, error: 'Parameter tidak lengkap' })
-      if (!global.apikey.includes(apikey))
-        return res.json({ status: false, error: 'Apikey invalid' })
+      if (!apikey || !text) return res.json({ status: false, error: 'Parameter tidak lengkap' })
+      if (!global.apikey.includes(apikey)) return res.json({ status: false, error: 'Apikey invalid' })
 
-      const pedo = await getBuffer(
+      const buffer = await getBuffer(
         `https://aqul-brat.hf.space/?text=${encodeURIComponent(text)}`
       )
 
       res.writeHead(200, {
         'Content-Type': 'image/webp',
-        'Content-Length': pedo.length
+        'Content-Length': buffer.length
       })
-      res.end(pedo)
+      res.end(buffer)
 
     } catch (error) {
       res.status(500).send(`Error: ${error.message}`)
     }
   })
 
-
   app.get('/imagecreator/bratvideo', async (req, res) => {
     try {
       const { apikey, text } = req.query
-      if (!apikey || !text)
-        return res.json({ status: false, error: 'Parameter tidak lengkap' })
-      if (!global.apikey.includes(apikey))
-        return res.json({ status: false, error: 'Apikey invalid' })
+      if (!apikey || !text) return res.json({ status: false, error: 'Parameter tidak lengkap' })
+      if (!global.apikey.includes(apikey)) return res.json({ status: false, error: 'Apikey invalid' })
 
-      const response = await fetch(
-        `https://brat.siputzx.my.id/gif?text=${encodeURIComponent(text)}`
-      )
-
-      const buffer = await response.buffer()
-      const contentType =
-        response.headers.get('content-type') || 'video/webm'
+      const url = `https://brat.siputzx.my.id/gif?text=${encodeURIComponent(text)}`
+      const buffer = (await axios.get(url, { responseType: 'arraybuffer' })).data
 
       res.writeHead(200, {
-        'Content-Type': contentType,
+        'Content-Type': 'image/gif',
         'Content-Length': buffer.length
       })
       res.end(buffer)
